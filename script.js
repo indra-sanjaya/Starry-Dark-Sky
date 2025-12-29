8e6e60915e91aef6a1a3664a909f8da2a297da64
indra-sanjaya <indrasanjaya.ind@gmail.com>
2025-12-29 19:54:00 +0700

    STARRY NIGHT

---
+++ script.js
@@ -0,0 +1,45 @@
+const canvas = document.getElementById("space")
+const ctx = canvas.getContext("2d")
+let w, h
+let speed = 5
+const stars = []
+
+function resize () {
+    w = canvas.width = window.innerWidth
+    h = canvas.height = window.innerHeight
+}
+
+window.addEventListener("resize", resize)
+resize()
+
+for (let i = 0; i < 1000; i++) {
+    stars.push({
+        x: Math.random() * w - w / 2,
+        y: Math.random() * h - h / 2,
+        z: Math.random() * w
+    })
+}
+
+document.addEventListener("mousemove", e => {
+    speed = (e.clientX / w) * 10
+})
+
+function animate() {
+    ctx.fillStyle = "rgba(0, 0, 0, 0.4)"
+    ctx.fillRect(0, 0, w, h)
+    ctx.fillStyle = "#fff"
+    stars.forEach(s => {
+        s.z -= speed
+        if (s.z <= 0) {
+            s.z = w
+        }
+        const x = (s.x / s.z) * w + w / 2
+        const y = (s.y / s.z) * h + h / 2
+        const size = (1 - s.z / w) * 3
+        ctx.beginPath()
+        ctx.arc(x, y, size, 0, Math.PI * 2)
+        ctx.fill()
+    })
+    requestAnimationFrame(animate)
+}
+animate()
